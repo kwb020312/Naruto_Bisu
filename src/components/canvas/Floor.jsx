0d6ff4d3ee14cd4ca1e4ad7c8487a0f23e9938bf
N S <kwb020312@naver.com>
2023-04-27 22:59:19 +0900

    Feat-중급닌자 시험장 추가

--- src/components/canvas/Floor.jsx
+++ src/components/canvas/Floor.jsx
@@ -1,8 +1,23 @@
+import { useGLTF } from "@react-three/drei";
+
 const Floor = () => {
+  const floor = useGLTF("./stage/scene.gltf");
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
-      <planeBufferGeometry args={[100, 100]} />
-      <meshPhongMaterial color={0xeeeeee} />
+    <mesh>
+      <spotLight
+        position={[5, 5, 25]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={floor.scene}
+        scale={1}
+        position={[0, -2, 0]}
+        rotation={[0, 9, 0]}
+      />
     </mesh>
   );
 };
